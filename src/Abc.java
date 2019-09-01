8649faef6c48b6ea514dce35c5093aee17264ec9
radhika0811 <radhika.agrawal_cs18@gla.ac.in>
2019-09-01 14:35:05 -0700

    another loop used

--- src/Abc.java
+++ src/Abc.java
@@ -1,7 +1,8 @@
 public class Abc {
     public static void main(String[] args) {
         for (int i = 1; i <= 9; i++) {
-            System.out.println(i);
+            for (int j = 1; j <= i; j++)
+                System.out.println(j);
         }
     }
 }
